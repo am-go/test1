d0a5153a28988188e2970ab48e836663b2c21654
chaeminlim <dlacoals1122@naver.com>
2019-05-09 20:04:21 +0900

    git practice chaemin

--- run.py
+++ run.py
@@ -2,12 +2,3 @@ from HelloAMGO import app
 
 app.run(host='0.0.0.0')
 
-
-<<<<<<< HEAD
-"reset all changes"
-=======
-"hello!"
-
-"i am in branch issues!"
-"helloworld"
->>>>>>> 0a89c6b471f42f218542d78550fe46ec54e90593
